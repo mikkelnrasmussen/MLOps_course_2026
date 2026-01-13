5222fbdbf95c862bb239c41309cd3433693f48af
mikkelnrasmussen <minra@dtu.dk>
2026-01-13 15:53:45 +0100

    fix: remove content for now

--- tests/test_data.py
+++
@@ -1,8 +0,0 @@
-from mlops_course.data import MyDataset
-from torch.utils.data import Dataset
-
-
-def test_my_dataset():
-    """Test the MyDataset class."""
-    dataset = MyDataset("data/raw")
-    assert isinstance(dataset, Dataset)
