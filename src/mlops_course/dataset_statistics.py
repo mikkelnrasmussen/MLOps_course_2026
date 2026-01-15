7006a63639c82a31397d92147b742df75dc6cdef
mikkelnrasmussen <minra@dtu.dk>
2026-01-15 16:06:00 +0100

    feat: add datasets statistics script

---
+++ src/mlops_course/dataset_statistics.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+import torch
+import typer
+from mpl_toolkits.axes_grid1 import ImageGrid
+
+from mlops_course.dataset import MnistDataset
+
+
+def main(datadir: str = "data") -> None:
+    """Compute dataset statistics."""
+    train_dataset = MnistDataset(data_folder=datadir, train=True)
+    test_dataset = MnistDataset(data_folder=datadir, train=False)
+    print(f"Train dataset: {train_dataset.name}")
+    print(f"Number of images: {len(train_dataset)}")
+    print(f"Image shape: {train_dataset[0][0].shape}")
+    print("\n")
+    print(f"Test dataset: {test_dataset.name}")
+    print(f"Number of images: {len(test_dataset)}")
+    print(f"Image shape: {test_dataset[0][0].shape}")
+
+    images = train_dataset.images[:25]
+    target = train_dataset.target[:25]
+    row_col = int(len(images) ** 0.5)
+    fig = plt.figure(figsize=(10.0, 10.0))
+    grid = ImageGrid(fig, 111, nrows_ncols=(row_col, row_col), axes_pad=0.3)
+    for ax, im, label in zip(grid, images, target):
+        ax.imshow(im.squeeze(), cmap="gray")
+        ax.set_title(f"Label: {label.item()}")
+        ax.axis("off")
+    plt.savefig("mnist_images.png")
+    plt.close()
+
+    train_label_distribution = torch.bincount(train_dataset.target)
+    test_label_distribution = torch.bincount(test_dataset.target)
+
+    plt.bar(torch.arange(10), train_label_distribution)
+    plt.title("Train label distribution")
+    plt.xlabel("Label")
+    plt.ylabel("Count")
+    plt.savefig("train_label_distribution.png")
+    plt.close()
+
+    plt.bar(torch.arange(10), test_label_distribution)
+    plt.title("Test label distribution")
+    plt.xlabel("Label")
+    plt.ylabel("Count")
+    plt.savefig("test_label_distribution.png")
+    plt.close()
+
+
+if __name__ == "__main__":
+    typer.run(main)
